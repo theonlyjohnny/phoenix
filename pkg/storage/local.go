37ee3b2c92e92ddbdfa4a1437d45386e3303e69c
Johnny Dallas <johnnydallas0308@gmail.com>
2019-07-03 08:43:33 +0000

    add mutex in local storage

--- pkg/storage/local.go
+++ pkg/storage/local.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"sync"
+
 	"github.com/theonlyjohnny/phoenix/internal/cluster"
 	"github.com/theonlyjohnny/phoenix/internal/instance"
 )
@@ -8,12 +10,14 @@ import (
 var (
 	instanceCache map[string]*instance.Instance
 	clusterCache  map[string]*cluster.Cluster
+	mutex         sync.RWMutex
 )
 
 //LocalStorage is an in-memory Storage implementation -- DO NOT USE IN PRODUCTION
 type LocalStorage struct {
 	instanceCache *map[string]*instance.Instance
 	clusterCache  *map[string]*cluster.Cluster
+	mutex         *sync.RWMutex
 }
 
 func init() {
@@ -25,21 +29,28 @@ func newLocalStorage() (*LocalStorage, error) {
 	return &LocalStorage{
 		&instanceCache,
 		&clusterCache,
+		&mutex,
 	}, nil
 }
 
 func (s LocalStorage) StoreInstance(i *instance.Instance) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	pkey := i.PhoenixID
 	(*s.instanceCache)[pkey] = i
 	return nil
 }
 
 func (s LocalStorage) DeleteInstance(phoenixID string) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	delete(*s.instanceCache, phoenixID)
 	return nil
 }
 
 func (s *LocalStorage) GetAllInstances() []*instance.Instance {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	instances := []*instance.Instance{}
 	for _, instance := range *s.instanceCache {
 		instances = append(instances, instance)
@@ -48,6 +59,8 @@ func (s *LocalStorage) GetAllInstances() []*instance.Instance {
 }
 
 func (s LocalStorage) ListClusters() []*cluster.Cluster {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
 	clusters := []*cluster.Cluster{}
 	for _, cluster := range *s.clusterCache {
 		clusters = append(clusters, cluster)
@@ -56,6 +69,8 @@ func (s LocalStorage) ListClusters() []*cluster.Cluster {
 }
 
 func (s LocalStorage) StoreCluster(cluster *cluster.Cluster) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	pkey := cluster.Name
 	(*s.clusterCache)[pkey] = cluster
 	return nil
